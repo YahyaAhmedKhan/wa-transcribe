7b95cd20a70d0f8a4d2eb75802b78da3e9227951
Yahya Ahmed Khan <yahyaahmed.52@gmail.com>
2025-03-02 18:00:56 +0500

    used eleven labs

--- api/whisper.js
+++ api/whisper.js
@@ -21,19 +21,31 @@ async function main() {
 }
 // main();
 
-export async function transcribeAudio(filepath){
-  // Create a transcription job
-  const transcription = await groq.audio.transcriptions.create({
-    file: fs.createReadStream(filepath),
-    model: "whisper-large-v3", // Required model to use for transcription
-    // model: "distil-whisper-large-v3-en", // Required model to use for transcription
-    // prompt: "Give the output in English wihtout traslating", // Optional
-    response_format: "text", // Optional
-    language: "en", // Optional
-    temperature: 0.0, // Optional
-  }); 
+// export async function transcribeAudio(filepath){
+//   // Create a transcription job
+//   const transcription = await groq.audio.transcriptions.create({
+//     file: fs.createReadStream(filepath),
+//     model: "whisper-large-v3", // Required model to use for transcription
+//     // model: "distil-whisper-large-v3-en", // Required model to use for transcription
+//     // prompt: "Give the output in English wihtout traslating", // Optional
+//     response_format: "text", // Optional
+//     language: "en", // Optional
+//     temperature: 0.0, // Optional
+//   }); 
 
-  return transcription;
+//   return transcription;
+// }
+
+
+export async function transcribeAudio(filepath){
+  const client = new ElevenLabsClient({ apiKey: process.env.ELEVENLABS_API_KEY });
+  const response = await client.speechToText.convert({
+      file: fs.createReadStream(filepath),
+      model_id: "scribe_v1",
+
+  });
+  return response.text;
 }
+
 
 // console.log(await transcribeAudio("aud.ogg"));
